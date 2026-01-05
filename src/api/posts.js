2c911418e59a768f82e244d4dbb2964cc2848c8f
Nishit41 <nishitranjan41@gmail.com>
2026-01-05 07:16:16 +0530

    Implement CreatePost functionality

--- src/api/posts.js
+++ src/api/posts.js
@@ -4,3 +4,12 @@ export const getPosts = async (queryParams) => {
     )
     return await response.json()
 }
+
+export const createPost = async (post) => {
+    const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/posts`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(post),
+    })
+    return await response.json()
+}
